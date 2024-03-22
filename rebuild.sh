d92b78a728e13e473aa38ea68d211883d135df44
pietruszka123 <56603721+pietruszka123@users.noreply.github.com>
2024-03-22 21:13:55 +0100

    Rebuild Script

---
+++ rebuild.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+abs_path=$(readlink -f .)
+
+NIX_PATH="$abs_path"
+
+command="switch"
+if ! [ -z "$1" ]
+  then
+    command=$1
+fi
+
+echo $command
+echo $abs_path
+
+nixos-rebuild switch --flake $NIX_PATH#default
