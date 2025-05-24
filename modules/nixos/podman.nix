86e697d12351ae613b9f104e6842624083f1f171
pietruszka123 <56603721+pietruszka123@users.noreply.github.com>
2025-05-24 19:32:20 +0200

    .

--- modules/nixos/podman.nix
+++ modules/nixos/podman.nix
@@ -13,16 +13,22 @@ in
     modules.podman.enable = lib.mkEnableOption "enable podman module";
   };
   config = lib.mkIf cfg.enable {
-    virtualisation.containers.enable = true;
     virtualisation = {
       podman = {
         enable = true;
         dockerCompat = true;
         defaultNetwork.settings.dns_enable = true;
       };
+      containers = {
+        enable = true;
+      };
     };
 
-    #environment.systemPackages = with pkgs; [ nvidia-podman ];
+    environment.systemPackages = with pkgs; [
+      dive
+      podman-tui
+      podman-compose
+    ];
 
   };
 }
